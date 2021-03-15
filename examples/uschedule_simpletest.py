20aab7a340ead8079be8f928c4f7914dfb16091f
Nathan Byrd <techy1@gmail.com>
2021-03-14 20:03:27 -0500

    Pylint changes for examples

--- examples/uschedule_simpletest.py
+++ examples/uschedule_simpletest.py
@@ -1,39 +1,39 @@
 # SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
 # SPDX-FileCopyrightText: Copyright (c) 2021 Nathan Byrd
-#
+
 # SPDX-License-Identifier: MIT
 
-import uschedule as schedule
 import time
+import uschedule as schedule
 
 
 def greet():
     print("Hello, world!")
 
 
-""" schedule every 10 seconds"""
+# schedule every 10 seconds
 schedule.every(10).seconds.do(greet)
 
-""" schedule every 10 minutes"""
+# schedule every 10 minutes
 schedule.every(10).minutes.do(greet)
 
-""" schedule once a day"""
+# schedule once a day
 schedule.every().day.at("10:30").do(greet)
 
-""" schedule from 5 to 10 minutes"""
+# schedule from 5 to 10 minutes
 schedule.every(5).to(10).minutes.do(greet)
 
-""" schedule on a particular day"""
+# schedule on a particular day
 schedule.every().monday.do(greet)
 
-""" schedule day and time"""
+# schedule day and time
 schedule.every().wednesday.at("13:15").do(greet)
 
-""" schedule once a minute at seventeen seconds"""
+# schedule once a minute at seventeen seconds
 schedule.every().minute.at(":17").do(greet)
 
 
 while True:
-    """ Run any pending jobs"""
+    # Run any pending jobs
     schedule.run_pending()
     time.sleep(1)
